791cab15a5e3903d4475117934394e65e98133fb
DucThang <ducthang0271555@gmail.com>
2025-03-23 19:35:23 +0700

    update

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from src.database import initialize_db
+from src.gui import start_gui
+
+def main():
+    print("🔹 Khởi tạo cơ sở dữ liệu...")
+    initialize_db()
+
+    print("🔹 Mở giao diện...")
+    start_gui()  # Chạy giao diện từ gui.py
+
+if __name__ == "__main__":
+    main()
